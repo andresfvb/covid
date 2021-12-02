a2f657ce6b73a580d5173bbb1909de0556626b4d
andresfvb <35276567+andresfvb@users.noreply.github.com>
2021-12-02 06:22:41 -0500

    punto 1

---
+++ covi-andres.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 19:46:25 2021
+@author: oscar perez
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
+
+# Punto 1
+
+num_pais = len(data)
+print("P")
+print(num_pais)
+print() 
+num_pais = len(data)
+print("P")
+print(num_pais)
+print()
