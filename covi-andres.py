ed4a4e4093e2126e0cead9f9e6a44f74c5fb98f4
andresfvb <35276567+andresfvb@users.noreply.github.com>
2021-12-02 22:22:33 -0500

    final

--- covi-andres.py
+++ covi-andres.py
@@ -174,3 +174,314 @@ print(tipoAtencion)
 print()
 
 #////////////////////////////////
+
+#////////////////////////////////
+#punto 12
+
+topDeptFa = data[data['Ubicación del caso'] == 'Fallecido'].groupby('Nombre departamento').size().sort_values(ascending = False).head(10)
+print("Punto 12")
+print(topDeptFa)
+print() 
+
+topDeptFa = data[data['Ubicación del caso'] == 'Fallecido'].groupby('Nombre departamento').size().sort_values(ascending = False).head(10)
+print("Punto 12")
+print(topDeptFa)
+print()
+
+#////////////////////////////////
+#punto 13
+
+topDeptRe = data[data['Recuperado'] == 'Recuperado'].groupby('Nombre departamento').size().sort_values(ascending = False).head(10)
+print("Punto 13")
+print(topDeptRe)
+print() 
+
+topDeptRe = data[data['Recuperado'] == 'Recuperado'].groupby('Nombre departamento').size().sort_values(ascending = False).head(10)
+print("Punto 13")
+print(topDeptRe)
+print() 
+print()
+
+#////////////////////////////////
+#punto 14
+
+topMunCo = data.groupby('Nombre municipio').size().sort_values(ascending = False).head(10)
+print("Punto 14")
+print(topMunCo)
+print()
+
+print(topMunCo)
+print()
+
+#////////////////////////////////
+#punto 15
+
+
+topMunFa = data[data['Ubicación del caso'] == 'Fallecido'].groupby('Nombre municipio').size().sort_values(ascending = False).head(10)
+print("Punto 15")
+print(topMunFa)
+print()
+
+topMunFa = data[data['Ubicación del caso'] == 'Fallecido'].groupby('Nombre municipio').size().sort_values(ascending = False).head(10)
+print("Punto 15")
+print(topMunFa)
+print()
+
+#////////////////////////////////
+#punto 16
+
+topMunRe = data[data['Recuperado'] == 'Recuperado'].groupby('Nombre municipio').size().sort_values(ascending = False).head(10)
+print("Punto 16")
+print(topMunRe)
+print()  
+
+topMunRe = data[data['Recuperado'] == 'Recuperado'].groupby('Nombre municipio').size().sort_values(ascending = False).head(10)
+print("Punto 16")
+print(topMunRe)
+print() 
+
+#////////////////////////////////
+#punto 17
+ciudadesCont = data.groupby(['Nombre departamento','Nombre municipio']).size().sort_values(ascending = False)
+print("Punto 17")
+print(ciudadesCont)
+
+ciudadesCont = data.groupby(['Nombre departamento','Nombre municipio']).size().sort_values(ascending = False)
+print("Punto 17")
+print(ciudadesCont)
+print()
+
+#////////////////////////////////
+#punto 18
+
+genCiudad = data.groupby(['Nombre departamento','Nombre municipio','Sexo']).size()
+print("Punto 18")
+print(genCiudad)
+print()
+
+genCiudad = data.groupby(['Nombre departamento','Nombre municipio','Sexo']).size()
+print("Punto 18")
+print(genCiudad)
+print()
+
+#////////////////////////////////
+#punto 19
+
+promCiudad = data.groupby(['Nombre departamento','Nombre municipio','Sexo']).Edad.mean()
+print("Punto 19")
+print(promCiudad)
+print()
+
+promCiudad = data.groupby(['Nombre departamento','Nombre municipio','Sexo']).Edad.mean()
+print("Punto 19")
+print(promCiudad)
+print()
+
+#////////////////////////////////
+#punto 20
+
+data['Nombre del país'].replace('ESTADOS UNIDOS','ESTADOS UNIDOS DE AMÉRICA',inplace=True)
+data['Nombre del país'].replace('VENEUELA','VENEZUELA',inplace=True)
+data['Nombre del país'].replace('MEXICO','MÉXICO',inplace=True)
+paisProc = data.groupby('Nombre del país').size().sort_values(ascending = False)
+print("Punto 20")
+print(paisProc)
+
+paisProc = data.groupby('Nombre del país').size().sort_values(ascending = False)
+print("Punto 20")
+print(paisProc)
+print()
+
+#////////////////////////////////
+#punto 21
+
+fechaContagios = data.groupby('Fecha de diagnóstico').size().sort_values(ascending = False)
+print("Punto 21")
+print(fechaContagios)
+print() 
+
+fechaContagios = data.groupby('Fecha de diagnóstico').size().sort_values(ascending = False)
+print("Punto 21")
+print(fechaContagios)
+print()
+
+#////////////////////////////////
+#punto 22
+
+tasaMor = (len(data[data['Ubicación del caso'] == 'Fallecido']) / len(data)) * 100
+tasaRec = (len(data[data['Recuperado'] == 'Recuperado']) / len(data)) * 100
+print("Punto 22")
+print(tasaMor)
+print(tasaRec)
+print() 
+
+print("Punto 22")
+print(tasaMor)
+print(tasaRec)
+print()
+
+#////////////////////////////////
+#punto 23
+
+tasaMorDept = (data[data['Ubicación del caso'] == 'Fallecido'].groupby('Nombre departamento').size().sort_values(ascending = False) / data[data['Ubicación del caso'] == 'Fallecido'].groupby('Nombre departamento').size().sort_values(ascending = False).sum()) * 100
+tasaRecDept = (data[data['Recuperado'] == 'Recuperado'].groupby('Nombre departamento').size().sort_values(ascending = False) / data[data['Recuperado'] == 'Recuperado'].groupby('Nombre departamento').size().sort_values(ascending = False).sum()) * 100
+print("Punto 23")
+print(tasaMorDept)
+print(tasaRecDept)
+print() 
+
+print("Punto 23")
+print(tasaMorDept)
+print(tasaRecDept)
+print()
+
+#////////////////////////////////
+#punto 24
+
+tasaMorCiu = (data[data['Ubicación del caso'] == 'Fallecido'].groupby('Nombre municipio').size().sort_values(ascending = False) / data[data['Ubicación del caso'] == 'Fallecido'].groupby('Nombre municipio').size().sort_values(ascending = False).sum()) * 100
+tasaRecCiu = (data[data['Recuperado'] == 'Recuperado'].groupby('Nombre municipio').size().sort_values(ascending = False) / data[data['Recuperado'] == 'Recuperado'].groupby('Nombre municipio').size().sort_values(ascending = False).sum()) * 100
+print("Punto 24")
+print(tasaMorCiu)
+print(tasaRecCiu)
+print() 
+
+print("Punto 24")
+print(tasaMorCiu)
+print(tasaRecCiu)
+print()
+
+#////////////////////////////////
+#punto 25
+atenCiu = data.groupby(['Nombre municipio','Ubicación del caso']).size()
+print("Punto 25")
+print(atenCiu)
+print() 
+atenCiu = data.groupby(['Nombre municipio','Ubicación del caso']).size()
+print("Punto 25")
+print(atenCiu)
+print() 
+print()
+
+
+#////////////////////////////////
+#punto 26
+
+promEdadCiu = data.groupby(['Nombre municipio','Sexo']).Edad.mean()
+print("Punto 26")
+print(promEdadCiu)
+print()
+
+print("Punto 26")
+print(promEdadCiu)
+print()
+
+#////////////////////////////////
+#punto 27
+
+data.groupby('Fecha de diagnóstico').size().cumsum().plot(label = "Contagios",figsize=(25,10))
+Fallecidos = data[data['Ubicación del caso'] == 'Fallecido']
+Fallecidos.groupby('Fecha de diagnóstico').size().cumsum().plot(label = "Fallecidos ",figsize=(25,10))
+Recuperado = data[data['Recuperado'] == 'Recuperado']
+Recuperado.groupby('Fecha de diagnóstico').size().cumsum().plot(label = "Recuperados",figsize=(25,10))
+plt.legend()
+plt.show() 
+
+data.groupby('Fecha de diagnóstico').size().cumsum().plot(label = "Contagios",figsize=(25,10))
+Fallecidos = data[data['Ubicación del caso'] == 'Fallecido']
+Fallecidos.groupby('Fecha de diagnóstico').size().cumsum().plot(label = "Fallecidos ",figsize=(25,10))
+Fallecidos.groupby('Fecha de diagnóstico').size().cumsum().plot(label = "Fallecidos",figsize=(25,10))
+Recuperado = data[data['Recuperado'] == 'Recuperado']
+Recuperado.groupby('Fecha de diagnóstico').size().cumsum().plot(label = "Recuperados",figsize=(25,10))
+plt.legend()
+plt.show()
+
+#////////////////////////////////
+#punto 28
+
+data.groupby('Nombre departamento').size().sort_values(ascending=False).head(10).plot(label = "Contagios",figsize=(20,10))
+Fallecidos = data[data['Ubicación del caso'] == 'Fallecido']
+Fallecidos.groupby('Nombre departamento').size().sort_values(ascending=False).head(10).plot(label = "Fallecidos",figsize=(20,10))
+Recuperado = data[data['Recuperado'] == 'Recuperado']
+Recuperado.groupby('Nombre departamento').size().sort_values(ascending=False).head(10).plot(label = "Recuperados",figsize=(20,10))
+plt.legend()
+plt.show() 
+
+Recuperado = data[data['Recuperado'] == 'Recuperado']
+Recuperado.groupby('Nombre departamento').size().sort_values(ascending=False).head(10).plot(label = "Recuperados",figsize=(20,10))
+plt.legend()
+plt.show()
+
+#////////////////////////////////
+#punto 29
+
+data.groupby('Nombre municipio').size().sort_values(ascending=False).head(10).plot(label = "Contagios",figsize=(20,10))
+Fallecidos = data[data['Ubicación del caso'] == 'Fallecido']
+Fallecidos.groupby('Nombre municipio').size().sort_values(ascending=False).head(10).plot(label = "Fallecidos",figsize=(20,10))
+Recuperado = data[data['Recuperado'] == 'Recuperado']
+Recuperado.groupby('Nombre municipio').size().sort_values(ascending=False).head(10).plot(label = "Recuperados",figsize=(20,10))
+plt.legend()
+plt.show() 
+
+Recuperado = data[data['Recuperado'] == 'Recuperado']
+Recuperado.groupby('Nombre municipio').size().sort_values(ascending=False).head(10).plot(label = "Recuperados",figsize=(20,10))
+plt.legend()
+plt.show() 
+plt.show()
+
+#////////////////////////////////
+#punto 30
+
+falleEdad = data[data['Ubicación del caso'] == 'Fallecido'].groupby('Edad').size().sort_values(ascending = False)
+print("Punto 30")
+print(falleEdad)
+print() 
+
+falleEdad = data[data['Ubicación del caso'] == 'Fallecido'].groupby('Edad').size().sort_values(ascending = False)
+print("Punto 30")
+print(falleEdad)
+print()
+
+#////////////////////////////////
+#punto 31
+
+porcAten = ((data.groupby('Ubicación del caso').size().sort_values(ascending = False)) / ((data.groupby('Ubicación del caso').size().sort_values(ascending = False)).sum())) * 100
+print("Punto 31")
+print(porcAten)
+print() 
+
+porcAten = ((data.groupby('Ubicación del caso').size().sort_values(ascending = False)) / ((data.groupby('Ubicación del caso').size().sort_values(ascending = False)).sum())) * 100
+print("Punto 31")
+print(porcAten)
+print() 
+print()
+
+#////////////////////////////////
+#punto 32
+
+data.groupby(['Ubicación del caso']).size().sort_values(ascending = False).plot(kind='bar') 
+data.groupby(['Ubicación del caso']).size().sort_values(ascending = False).plot(kind='bar') 
+data.groupby(['Ubicación del caso']).size().sort_values(ascending = False).plot(kind='bar')
+
+#////////////////////////////////
+#punto 33
+
+data.groupby(['Sexo']).size().sort_values(ascending = False).plot(kind='bar') 
+data.groupby(['Sexo']).size().sort_values(ascending = False).plot(kind='bar') 
+data.groupby(['Sexo']).size().sort_values(ascending = False).plot(kind='bar')
+
+#////////////////////////////////
+#punto 34
+
+data.groupby(['Tipo de contagio']).size().sort_values(ascending = False).plot(kind='bar') 
+
+data.groupby(['Tipo de contagio']).size().sort_values(ascending = False).plot(kind='bar')
+
+#////////////////////////////////
+#punto 35
+
+data.groupby('Fecha de diagnóstico').size().plot(kind = 'bar',facecolor = 'red',figsize=(25,10))
+Fallecidos = data[data['Ubicación del caso'] == 'Fallecido']
+Fallecidos.groupby('Fecha de diagnóstico').size().plot(kind = 'bar', facecolor = 'blue',figsize=(25,10))
+Recuperado = data[data['Recuperado'] == 'Recuperado']
+Recuperado.groupby('Fecha de diagnóstico').size().plot(kind = 'bar', facecolor = 'green',figsize=(25,10))
+plt.legend(["Contagiados", "Fallecidos", "Recuperados"])
